75905cb1a75bc9f14f9d5cdd1961ec48f4631059
Danut Dumitru <danut.dumitru@gmail.com>
2020-08-21 14:46:49 +0300

    in work

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -12,7 +17,13 @@ class FlashChat extends StatelessWidget {
           bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.ID,
+      routes: {
+        ChatScreen.ID: (context) => ChatScreen(),
+        LoginScreen.ID: (context) => LoginScreen(),
+        RegistrationScreen.ID: (context) => RegistrationScreen(),
+        WelcomeScreen.ID: (context) => WelcomeScreen(),
+      },
     );
   }
 }
